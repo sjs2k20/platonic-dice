6c61a4e5002b53196629115f254ca9bfdb196b58
sjs2k20 <196786506+sjs2k20@users.noreply.github.com>
2025-09-27 04:45:23 +0100

    Runtime type safety in Die.js
    Enforce typings or throw Error.

--- src/core/dice/Die.js
+++ src/core/dice/Die.js
@@ -8,6 +8,9 @@ class Die {
      * @param {DieType} type - The type of die.
      */
     constructor(type) {
+        if (!Object.values(DieType).includes(type)) {
+            throw new Error(`Invalid die type: ${type}`);
+        }
         this._type = type;
         this._result = null;
         this._history = [];
@@ -30,6 +33,12 @@ class Die {
      * @returns {number} - The roll result.
      */
     roll(rollType = null) {
+        if (
+            rollType !== null &&
+            !Object.values(RollType).includes(rollType)
+        ) {
+            throw new Error(`Invalid roll type: ${rollType}`);
+        }
         this._reset();
         this._result = rollDie(this._type, rollType);
         this._history.push(this._result);
@@ -82,6 +91,7 @@ class Die {
      * Converts the die's state to a JSON string.
      * Includes full history by default.
      *
+     * @param {boolean} [verbose=true] - If true, includes history.
      * @returns {string} - A JSON-formatted string representing the die's state.
      */
     toJSON(verbose = true) {
